2379613802ae6808c00ee6091b449d183afa0f98
Igor Matsuoka <igor.mtka@gmail.com>
2022-01-10 15:48:07 -0300

    coordinator

--- src/Coordinators/Coordinator.js
+++ src/Coordinators/Coordinator.js
@@ -1,2 +1,31 @@
+export const goToHome = (history) => {
+    history.push("/")
+}
 
+export const goToLogin = (history) => {
+    history.push("/login")
+}
 
+export const goToEditAdress = (history) => {
+    history.push("/editarendereco")
+}
+
+export const goToEditProfile = (history) => {
+    history.push("/editarperfil")
+}
+
+export const goToProfile = (history) => {
+    history.push("/perfil")
+}
+
+export const goToRestaurant = (history, id) => {
+    history.push(`/restaurante/${id}`)
+}
+
+export const goToSearch = (history) => {
+    history.push("/buscar")
+}
+
+export const goToSignUp = (history) => {
+    history.push("/cadastrar")
+}
