8c0075757d626538a5b712adf7b31055d0a07209
Stefani <stefani.barros28@gmail.com>
2022-01-11 10:28:27 -0300

    adicionando página de login

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "./LoginForm";
 
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/Services/Access.js
+++ src/Services/Access.js
@@ -1,18 +1,33 @@
-import axios from "axios"
-import { BASE_URL } from "../Constants/URL"
-import { goToHome } from "../Routes/Coordinator"
+import axios from "axios";
+import { BASE_URL } from "../Constants/URL";
+import { goToHome, goToSignUp } from "../Routes/Coordinator";
 
+export const Login = (body, history) => {
+  axios
+    .post(`${BASE_URL}/login`, body)
+    .then((res) => {
+      localStorage.getItem("token", res.data.token);
+      goToHome(history);
+    })
+    .catch((err) => {
+      alert(err.response.data.message);
+      goToSignUp(history);
+    });
+};
 
 export const SignUp = (body, clear, history) => {
-    axios.post(`${BASE_URL}/signup`, body)
-    .then((res)=>{
-        localStorage.setItem("token", res.data.token)
-        alert("Cadastro Realizado com sucesso!")
-        clear()
-        goToHome(history)
+  axios
+    .post(`${BASE_URL}/signup`, body)
+    .then((res) => {
+      localStorage.setItem("token", res.data.token);
+      alert("Cadastro Realizado com sucesso!");
+      clear();
+      goToHome(history);
     })
-    .catch((err)=>{
-        console.log(err)
-        alert("Possiveis erros: senha com menos de 8 caracteres, e-mail já criado, senha igual a de outro usuário!")
-    })
-}
+    .catch((err) => {
+      console.log(err);
+      alert(
+        "Possiveis erros: senha com menos de 8 caracteres, e-mail já criado, senha igual a de outro usuário!"
+      );
+    });
+};
