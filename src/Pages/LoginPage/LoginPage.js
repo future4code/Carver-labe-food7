2fdc5340883d6d8e9087a3167b48c2bc30d4c42c
Igor Matsuoka <igor.mtka@gmail.com>
2022-01-12 13:55:39 -0300

    protegendo login

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import LoginForm from "./LoginForm";
 import { LogoImage, TextSingUp } from "./styled";
+import { useUnprotectedPage } from "../../Hooks/useUnprotectedPage"
 
 const LoginPage = () => {
+  useUnprotectedPage()
   return (
     <div>
       <LogoImage src={"https://cdn.zeplin.io/5dd5ae92669af1bc817c8359/assets/2996043A-2111-4F86-AB91-37474F98621C.svg"} />
